d912c6d2338494e1e74820b3c0042c526d43fbbb
liumu96 <918883717@qq.com>
2024-07-21 13:48:59 +0800

    Fix bugs

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -4,6 +4,14 @@ import ChewingTesting from "@/components/ChewingTesting";
 import { VideoProvider } from "@/context/VideoContext";
 import EyeJawDistanceChart from "@/components/EyeJawDistanceChart";
 import Resizer from "@/components/Resizer";
+import dynamic from "next/dynamic";
+// Dynamically import the ChewingTesting component with SSR disabled
+const ChewingTestingNoSSR = dynamic(
+  () => import("@/components/ChewingTesting"),
+  {
+    ssr: false, // This disables server-side rendering for the component
+  }
+);
 
 const TestingPage = () => {
   const [topHeight, setTopHeight] = useState((2 / 3) * window.innerHeight);
@@ -22,7 +30,7 @@ const TestingPage = () => {
     // <VideoProvider>
     <div className="w-full min-h-screen flex flex-col">
       <div style={{ height: `${topHeight}px` }} className="relative">
-        <ChewingTesting />
+        <ChewingTestingNoSSR />
       </div>
       <Resizer onResize={handleResize} />
       <div style={{ height: `${bottomHeight}px` }} className="relative">
